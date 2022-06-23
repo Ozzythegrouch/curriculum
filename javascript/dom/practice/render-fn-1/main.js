cdae3949b10232e4552e5a5989902c9c49cbf2ec
Oscar Trejo <iamoscartrejo@gmail.com>
2022-06-23 14:46:23 -0700

    add practice for render fn 1

---
+++ javascript/dom/practice/render-fn-1/main.js
@@ -0,0 +1,22 @@
+
+const root = document.getElementById('root');
+
+const renderDiv = function(){
+    const div = document.createElement('div');
+    div.innerText = 'I am a new div rendered with a function expression.';
+    root.append(div);
+}
+
+const btn = document.createElement('button');
+btn.innerText = 'Click Me';
+btn.addEventListener('click', renderDiv);
+root.append(btn);
+
+// const btn = document.createElement('button');
+// btn.innerText = 'Click Me';
+// btn.addEventListener('click', function(){
+//     const div = document.createElement('div');
+//     div.innerText = 'I am a new div';
+//     root.append(div);
+// });
+// root.append(btn);
